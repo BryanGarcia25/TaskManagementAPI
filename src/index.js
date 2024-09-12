7d5d2e334b02f8fceb26de565a28b9e3e8bbf689
Bryan Garcia <osvaldo52programacion@gmail.com>
2024-09-12 00:50:46 -0700

    Configure Express Server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('API funcionando correctamente');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
