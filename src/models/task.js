289a4bacbb6ad8d43da0b2867c8fa2e17c092997
Bryan Garcia <osvaldo52programacion@gmail.com>
2024-09-13 02:00:32 -0700

    Create task model

---
+++ src/models/task.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const taskScheme = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    dueDate: { type: Date, required: true },
+    completed: { type: Boolean, default: false },
+});
+
+export const Task = mongoose.model('Task', taskScheme);
