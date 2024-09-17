fc60342df395fead6577f3a37a8a060a637c877b
Bryan Garcia <osvaldo52programacion@gmail.com>
2024-09-16 18:01:50 -0700

    Create logic for login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+const generateAccessToken = require('../utils/generateToken');
 
 exports.registerUser = async (req, res) => {
     const { username, email, password } = req.body;
@@ -8,6 +9,27 @@ exports.registerUser = async (req, res) => {
         const newUser = new User({ username, email, password: hashedPassword });
         await newUser.save();
         res.status(201).json( { message: 'Usuario registrado correctamente' } );
+    } catch (error) {
+        res.status(500).json( { message: 'Servidor fuera de servicio' } );
+    }
+}
+
+exports.login = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const userFound = await User.findOne({ username });
+        if (!userFound) {
+            return res.status(404).json( { message: 'El usuario no existe en la base de datos...' } );
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, userFound.password);
+        if (!isPasswordMatch) {
+            return res.status(401).json( { message: 'La contraseña es incorrecta, verifique de nuevo...' } );
+        }
+
+        const token = generateAccessToken.token(userFound._id);
+        res.json({ token });
     } catch (error) {
         res.status(500).json( { message: 'Servidor fuera de servicio' } );
     }
