1f8f84e46d0a6548143eaed3cbad74cb16ff1398
Bryan Garcia <osvaldo52programacion@gmail.com>
2024-09-19 01:50:34 -0700

    Add CORS configuration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const authRoutes = require('./routes/authRoutes')
 const taskRoutes = require('./routes/taskRoutes')
+const cors = require('cors');
 require('dotenv').config();
 require('./config/db');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:4200',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 app.use('/auth', authRoutes);
 app.use('/api', taskRoutes);
 
